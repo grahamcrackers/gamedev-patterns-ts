fa2fca5c1e8627d6f8859f8b9c64068106dacbb0
Graham Rogers <graham.rogers4@pm.me>
2021-04-14 07:59:10 -0700

    :construction: middle of part 4

---
+++ src/utils/color/color.ts
@@ -0,0 +1,7 @@
+export class Color {
+    public readonly R: number;
+    public readonly G: number;
+    public readonly B: number;
+    public readonly A: number;
+    constructor(r: number, g: number, b: number, a: number) {}
+}
